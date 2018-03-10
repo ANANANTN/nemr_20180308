cf2eac77258ac34ecf346b293555a88d2e0dbc59
ananantn <nortonanthonya@gmail.com>
2018-03-09 19:56:27 -0500

    Add OAuth for google, confirm it is connecting to google

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: '75578717757-7738tqli4odom2dkraurbrfa48pubm1a.apps.googleusercontent.com',
+  googleClientSecret: 'KoL6BtQ4cv3hoO2hBSx1CeHh',
+};
--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
+
+// new GoogleStrategy() // creates a new instance of the Google GoogleStrategy
+// passport.use(...) // Tells passport that a strategy is available (in this
+// case the google strategy)
+// We must passs a client ID and client secret to GoogleStrategy to use it.
+// We get the ID and secret from google. So we must make a call to Google.
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+    },
+  ),
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send('<a href="./auth/google">Go to /auth/google.</a>');
 });
 
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}.`);
 });
