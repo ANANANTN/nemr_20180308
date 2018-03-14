6ebb885651c6d5fb30f4fda1c58b3216c4ab763b
ananantn <nortonanthonya@gmail.com>
2018-03-13 22:59:16 -0400

    Add handling prod vs dev for keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
-module.exports = {
-  googleClientID:
-    '75578717757-7738tqli4odom2dkraurbrfa48pubm1a.apps.googleusercontent.com',
-  googleClientSecret: 'KoL6BtQ4cv3hoO2hBSx1CeHh',
-  mongoURI:
-    'mongodb://anthony:WAY2fast4Me@ds211029.mlab.com:11029/emaily-dev-20180311',
-  cookieKey: 'oiwrgtsnvkjdnfgbdiotd0429uq345jioaegr0u(98U(*Y*&$^%%76))',
-};
+// keys.js - figure out what sets of keys to return
+if (process.env.NODE_ENV === 'production') {
+  // we are in production - return the production keys
+  module.exports = require('./prod');
+} else {
+  // we are in development - return development keys.
+  module.exports = require('./dev');
+}
